3ef7a35fc8f55b249b2e11f3d0e31168b36f0b3b
sarvamangalaV <121847248+sarvamangalaV@users.noreply.github.com>
2023-03-17 18:31:50 +0530

    Add files via upload

---
+++ my name.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class MyClass{
+    public static void Main()
+    {
+        var name="sarvamangala";
+
+        Console.WriteLine($"hello this is {name}");
+        Console.WriteLine();
+    }
+}
+    
+    
+        
+
